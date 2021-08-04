0460717431af2510c522dddd77e1c5a66447cd32
FJHHH <1375006685@qq.com>
2021-08-04 16:57:11 +0800

    binary-search

--- src/main/kotlin/704/Solution.kt
+++ src/main/kotlin/704/Solution.kt
@@ -3,7 +3,32 @@ package `704`
 // https://leetcode-cn.com/problems/binary-search/
 
 class Solution {
-//    fun search(nums: IntArray, target: Int): Int {
-//
-//    }
+    fun search(nums: IntArray, target: Int): Int {
+        var l = 0
+        var r = nums.lastIndex
+
+        while (l <= r) {
+            val mid = (l + r) / 2
+            val midNum = nums[mid]
+            if (midNum == target) {
+                return mid
+            }
+            if (midNum < target) {
+                l = mid + 1
+            } else {
+                r = mid - 1
+            }
+        }
+        if (nums[l] == target) {
+            return l
+        }
+        if (nums[r] == target) {
+            return r
+        }
+        return -1
+    }
+}
+
+fun main() {
+    println(Solution().search(intArrayOf(-1, 0, 3, 5, 9, 12), 9))
 }
