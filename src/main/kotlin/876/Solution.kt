6c8f8547cabf389878ba7423dfc28f3c7dbd8b2f
FJHHH <1375006685@qq.com>
2021-11-10 00:29:23 +0800

    submit 876

---
+++ src/main/kotlin/876/Solution.kt
@@ -0,0 +1,64 @@
+package `876`
+
+//Given the head of a singly linked list, return the middle node of the linked
+//list. 
+//
+// If there are two middle nodes, return the second middle node. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: head = [1,2,3,4,5]
+//Output: [3,4,5]
+//Explanation: The middle node of the list is node 3.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: head = [1,2,3,4,5,6]
+//Output: [4,5,6]
+//Explanation: Since the list has two middle nodes with values 3 and 4, we 
+//return the second one.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in the list is in the range [1, 100]. 
+// 1 <= Node.val <= 100 
+// 
+// Related Topics 链表 双指针 👍 431 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+class Solution {
+    fun middleNode(head: ListNode?): ListNode? {
+        var l = head
+        var r = head?.next
+
+        while (r != null) {
+            l = l!!.next
+            r = r.next
+            r = r?.next
+        }
+        return l
+    }
+}
+
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
